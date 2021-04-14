9e6529c7da455adf5659faf2963696833633f7b3
Jakub <kumroz@gmail.com>
2021-04-14 20:38:00 +0200

    personal finances - transaction list added, basic styling of transaction Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _MyAppState();
+    return _MyHomePage();
   }
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyHomePage extends State<MyApp> {
+  final List<Transaction> transactions = [
+    Transaction('t1', "Shoes", 249.99, DateTime.now()),
+    Transaction('t2', "Snacks", 6.49, DateTime.now()),
+    Transaction('t3', "Groceries", 36.99, DateTime.now()),
+    Transaction('t4', "T-shirt", 59.99, DateTime.now()),
+    Transaction('t5', "Uber eats", 35.99, DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +27,65 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Personal Finance App'),
         ),
-        body: Text('To do'),
+        body: Column(
+          children: <Widget>[
+            Card(
+              color: Colors.blue,
+              child: Container(width: double.infinity, child: Text("chart")),
+              elevation: 5,
+            ),
+            Column(
+              children: transactions
+                  .map((tx) => Card(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 15,
+                              ),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.purple,
+                                  width: 2,
+                                ),
+                              ),
+                              child: Text(
+                                '${tx.amount} PLN',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Colors.purple,
+                                ),
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  tx.title,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  DateFormat.yMd().format(tx.dateTime),
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            )
+          ],
+        ),
       ),
     );
   }
